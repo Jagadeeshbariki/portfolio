bb31e9729ce2aefd10689b9bd92196c7b73effd5
Jaga_2000 <111977705+Jagadeeshbariki@users.noreply.github.com>
2024-07-04 14:38:18 +0000

    Your descriptive commit message

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Bariki Jagadeesh",
+  title: "Web Designer & Web Developer",
+  email: "vyomanautjagadeesh@gmail.com",
+  gitHub: "https://github.com/Jagadeeshbariki",
+  instagram: "https://www.instagram.com/vyomanautjagadeesh/",
+  linkedIn: "satyahttps://www.linkedin.com/in/b-jagadeesh-883b1122a/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
